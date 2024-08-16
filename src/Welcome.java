ed51be11ee7b44293f88a6f5879f31e730d1a7db
lanhuong111204 <22002095.huong@student.iuh.edu.vn>
2024-08-16 22:17:46 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Huong Doan");
+        System.out.println("Welcome to the DHHTTT19A!");
+
+    }
+}
